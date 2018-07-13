4bcbd39a263916aa4459e448173339480bd31d7e
bailongctui <bailongctui@gmail.com>
2018-07-13 17:54:14 +0800

    express mock

--- config/index.js
+++ config/index.js
@@ -15,7 +15,7 @@ module.exports = {
         changeOrigin: true,// 如果接口跨域，需要进行这个参数配置
         target: 'http://localhost:3000',// 接口的域名
         pathRewrite: {
-          '^/api': ''//后面可以使重写的新路径，一般不做更改
+          '^/': ''//后面可以使重写的新路径，一般不做更改
         }
       }
     },
@@ -49,6 +49,7 @@ module.exports = {
     cacheBusting: true,
 
     cssSourceMap: true
+
   },
 
   build: {
