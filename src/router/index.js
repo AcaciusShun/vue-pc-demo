effa073b3c60c93d3f4f2b1f5fba51639646b1fa
bailongctui <bailongctui@gmail.com>
2018-07-17 14:24:40 +0800

    detail-layout
    详情页布局

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import IndexPage from './../pages/index'
 import VueSource from 'vue-resource'
+import DetailPage from '../pages/detail';
 
 Vue.use(Router)
 Vue.use(VueSource)
@@ -13,6 +14,11 @@ export default new Router({
       path: '/',
       name: 'index',
       component: IndexPage
+    },
+    {
+      path: '/detail',
+      name: 'detail',
+      component: DetailPage
     }
   ]
 })
